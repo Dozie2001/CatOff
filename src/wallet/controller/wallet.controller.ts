4228753b730b0d8d100d6abb7af13212a336cff7
Dozie2001 <davidchidozie402@gmail.com>
2024-06-08 13:41:58 +0100

    Set Up retrieving all wallets for a user

--- src/wallet/controller/wallet.controller.ts
+++ src/wallet/controller/wallet.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Post, Get, Delete, Put } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete, Put } from '@nestjs/common';
 import { WalletService } from '../services/wallet.service';
-
+import { Wallet } from '../services/models/wallet.interface';
+import { User } from 'src/users/models/user.class';
+import { Observable } from 'rxjs';
 @Controller('wallet')
 export class WalletController {
 
+    constructor( private walletService: WalletService) {}
 
     @Post('user')
-    create()  {
-
+    create(@Body() user: User, post: Wallet ): Observable<Wallet>  {
+        return this.walletService.createWallet(user, post);
     }
 
     @Get('user/id')
--- src/wallet/services/wallet.service.ts
+++ src/wallet/services/wallet.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { WalletEntity } from './models/wallet.entity';
+import { User } from 'src/users/models/user.class';
+import { UserEntity } from 'src/users/models/user.entity';
+import { Wallet } from './models/wallet.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { from, Observable } from 'rxjs';
+import cryptoRandomString from 'crypto-random-string';
+
+
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+    constructor(
+        @InjectRepository(WalletEntity)
+        private readonly walletrepository: Repository<WalletEntity>
+    ) {}
+
+    createWallet(user: User, wallet: Wallet): Observable<Wallet> {
+        wallet.author = user;
+        wallet.address = this.generateRandomWalletAddress();
+        return from(this.walletrepository.save(wallet));
+    }
+
+    // Retrieves all wallets related to a particylar user
+    findAllwallets(): Observable<Wallet[]> {
+        return from(
+            this.walletrepository
+                .createQueryBuilder('wallet')
+                .innerJoinAndSelect('wallet.author', 'author')
+                .getMany()
+        )
+    }
+
+    // Create wallet Address
+    generateRandomWalletAddress() {
+        return `0x${cryptoRandomString({length: 20, type: 'base64'})}`
+    }
+}
